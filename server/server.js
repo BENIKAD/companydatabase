e9a528299eec2de0073d85a77a193f6d19af1371
Benikad <bikuesan9@gmail.com>
2024-12-21 19:24:19 +0100

    this is new

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    credentials: true, // Allow cookies or authentication headers(for authetication)
+    credentials: true, // Allow cookies or authenticatio headers(for authetication) 
   }));
   
 app.use(express.json());
